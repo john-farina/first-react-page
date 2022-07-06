ec22da3085e995bb2dc60497652a19f7a1a965f4
onefootfarina <clichecontent@gmail.com>
2022-07-05 20:41:27 -0400

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './index.css';
 function App() {
     return (
         <>
-            <h1 className="swag">hello</h1>
+            <h1 className="swag">hello test</h1>
         </>
     );
 }
